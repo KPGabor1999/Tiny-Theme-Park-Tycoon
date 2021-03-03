ddcdd253520237c000c43a61ae0ca4e1d1d4a8e6
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-03-03 04:23:09 +0100

    Added skeleton for park and tiles

---
+++ src/main/java/Idlethemeparkworld/model/Park.java
@@ -0,0 +1,43 @@
+package Idlethemeparkworld.model;
+
+public class Park {
+    private int funds;
+    private Tile[][] tiles;
+    
+    public Park(){
+        tiles = new Tile[10][10];
+        initializePark();
+    }
+    
+    public Park(int size){
+        tiles = new Tile[size][size];
+        initializePark();
+    }
+    
+    private void initializePark(){
+        /*  1.Make sure all tiles are empty
+            2.Spawn in the gate tile
+        */
+    }
+    
+    public Building getBuildings(){
+        return null;
+    }
+    
+    public Attraction getRandomAttraction(){
+        return null;
+    }
+    
+    public Building findBuilding(){
+        return null;
+    }
+    
+    //Build type will be an enum
+    public boolean canBuild(int buildType){
+        return true; 
+    }
+    
+    public void build(){
+        
+    }
+}
---
+++ src/main/java/Idlethemeparkworld/model/Tile.java
@@ -0,0 +1,23 @@
+package Idlethemeparkworld.model;
+
+public class Tile {
+    private int x, y;
+    private boolean isBase;
+    private Building building;
+    
+    public Tile(){
+        
+    }
+    
+    public Tile(int x, int y){
+        
+    }
+    
+    public void setBuilding(boolean isBase){
+        
+    }
+    
+    public void unsetBuilding(){
+        
+    }
+}
