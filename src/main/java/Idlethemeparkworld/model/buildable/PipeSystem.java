4c7888cb40c9c657b57afc15957b1a6d3a3eca84
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-03-04 02:01:07 +0100

    Added skeleton for infrastucture buildings and systems

---
+++ src/main/java/Idlethemeparkworld/model/buildable/PipeSystem.java
@@ -0,0 +1,30 @@
+package Idlethemeparkworld.model.buildable;
+
+public class PipeSystem extends Buildable {
+    private int capacity;
+    private int load;
+    
+    public void canFlush(){
+        
+    }
+    
+    public void addConstantLoad(int amount){
+        
+    }
+    
+    public void removeConstantLoad(int amount){
+        
+    }
+    
+    public void addTemporaryLoad(int amount, int duration){
+        
+    }
+    
+    public void build(){
+        
+    }
+    
+    public void upgrade(){
+        
+    }
+}
