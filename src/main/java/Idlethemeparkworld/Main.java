801a0d1b6c34163c828a2a2f4c21a82e933cde8c
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-03-04 15:23:55 +0100

    Added top menubar to UI

--- src/main/java/Idlethemeparkworld/Main.java
+++ src/main/java/Idlethemeparkworld/Main.java
@@ -1,7 +1,15 @@
 package Idlethemeparkworld;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.AbstractAction;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.WindowConstants;
 
 public class Main extends JFrame{  
@@ -11,6 +19,38 @@ public class Main extends JFrame{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         //URL url = MainWindow.class.getClassLoader().getResource("assets/icon.png");
         //setIconImage(Toolkit.getDefaultToolkit().getImage(url));
+        
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuGame = new JMenu("Game");
+
+        JMenuItem menuNewGame = new JMenuItem(new AbstractAction("New game") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //startNewGame();
+            }
+        });
+        
+        JMenuItem menuHighScores = new JMenuItem(new AbstractAction("Leaderboards") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //
+            }
+        });
+        
+        JMenuItem menuGameExit = new JMenuItem(new AbstractAction("Exit") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        
+        menuGame.add(menuNewGame);
+        menuGame.add(menuHighScores);
+        menuGame.addSeparator();
+        menuGame.add(menuGameExit);
+        menuBar.add(menuGame);
+        setJMenuBar(menuBar);
         
         setLocationRelativeTo(null);
         //pack();
