532f5809ce940cdc484db729f7ccea0038143876
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-03-05 03:41:58 +0100

    Added skeleton for top information bar

--- src/main/java/Idlethemeparkworld/Main.java
+++ src/main/java/Idlethemeparkworld/Main.java
@@ -1,18 +1,25 @@
 package Idlethemeparkworld;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.AbstractAction;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 public class Main extends JFrame{  
+    private final JLabel timeLabel; 
+    private final JLabel moneyLabel; 
+    private final JLabel visitorCountLabel;
+    private final JLabel happinessLabel; 
     public Main() throws IOException{
         setTitle("Idle Theme Park World");
         setSize(600, 600);
@@ -43,7 +50,6 @@ public class Main extends JFrame{
                 System.exit(0);
             }
         });
-
         
         menuGame.add(menuNewGame);
         menuGame.add(menuHighScores);
@@ -51,6 +57,34 @@ public class Main extends JFrame{
         menuGame.add(menuGameExit);
         menuBar.add(menuGame);
         setJMenuBar(menuBar);
+        
+        setLayout(new BorderLayout(0, 10));
+        
+        /*---------------------------------------------------------*/
+        
+        timeLabel = new JLabel("time");
+        moneyLabel = new JLabel("money");
+        visitorCountLabel = new JLabel("visitor");
+        happinessLabel = new JLabel("happiness");
+        
+        timeLabel.setText("time:0");
+        timeLabel.setForeground(Color.cyan);
+        moneyLabel.setText("money:1000");
+        moneyLabel.setForeground(Color.GREEN);
+        visitorCountLabel.setText("visitor:20");
+        visitorCountLabel.setForeground(Color.RED);
+        happinessLabel.setText("happiness:100%");
+        happinessLabel.setForeground(Color.YELLOW);
+
+        JPanel informationPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        ((FlowLayout)informationPanel.getLayout()).setHgap(50);
+        informationPanel.setBackground(Color.darkGray);
+        informationPanel.add(timeLabel);
+        informationPanel.add(moneyLabel);
+        informationPanel.add(visitorCountLabel);
+        informationPanel.add(happinessLabel);
+        
+        add(informationPanel, BorderLayout.NORTH);
         
         setLocationRelativeTo(null);
         //pack();
