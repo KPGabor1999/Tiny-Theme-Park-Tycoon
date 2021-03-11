65f1338b9f77d0c3f77ececef9c5fb23a2b3d18c
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-03-11 01:45:24 +0100

    Added static information to enum

--- src/main/java/Idlethemeparkworld/model/BuildType.java
+++ src/main/java/Idlethemeparkworld/model/BuildType.java
@@ -14,24 +14,29 @@ import Idlethemeparkworld.model.buildable.infrastucture.Toilet;
 import Idlethemeparkworld.model.buildable.infrastucture.TrashCan;
 
 public enum BuildType {
-    PAVEMENT(1,1),
-    TRASHCAN(1,1),
-    TOILET(1,1),
-    HOTDOGSTAND(1,1),
-    ICECREAMPARLOR(1,1),
-    BURGERJOINT(1,1),
-    CAROUSEL(1,1),
-    FERRISWHEEL(1,1),
-    SWINGINGSHIP(1,1),
-    ROLLERCOASTER(1,1),
-    HAUNTEDMANSION(1,1),
-    ENTRANCE(1,1);
+    PAVEMENT(1,1,"Pavement",1000,0),
+    TRASHCAN(1,1,"Trash can",1000,0),
+    TOILET(1,1,"Toilet",5000,0),
+    HOTDOGSTAND(1,1,"Hotdog stand",10000,0),
+    ICECREAMPARLOR(1,1,"Icecream parlor",15000,0),
+    BURGERJOINT(1,1,"Burger joint",25000,0),
+    CAROUSEL(1,1,"Carousel",15000,0),
+    FERRISWHEEL(1,1,"Ferriswheel",25000,0),
+    SWINGINGSHIP(1,1,"Swinging ship",20000,0),
+    ROLLERCOASTER(1,1,"Roller doaster",35000,0),
+    HAUNTEDMANSION(1,1,"Haunted mansion",20000,0),
+    ENTRANCE(1,1,"Entrance",0,0);
     
     private final int width, length;
+    private final String name;
+    private final int buildCost, upgradeCost;
     
-    BuildType(int w, int l){
+    BuildType(int w, int l, String name, int bc, int uc){
         this.width = w;
         this.length = l;
+        this.name = name;
+        this.buildCost = bc;
+        this.upgradeCost = uc;
     }
     
     public static Class GetClass(BuildType type) {
