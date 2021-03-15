dbcc2bec22820c04ba2351a204a1a8a4d3a8907d
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-03-15 04:02:29 +0100

    Agent types and static consts added

---
+++ src/main/java/Idlethemeparkworld/model/agent/Agent.java
@@ -0,0 +1,26 @@
+package Idlethemeparkworld.model.agent;
+
+public class Agent {
+    private static final int AGENT_MAX_THOUGHTS = 5;
+    private static final int AGENT_THOUGHT_ITEM_NONE = 255;
+
+    private static final int AGENT_HUNGER_WARNING_THRESHOLD = 25;
+    private static final int AGENT_THIRST_WARNING_THRESHOLD = 25;
+    private static final int AGENT_TOILET_WARNING_THRESHOLD = 28;
+    private static final int AGENT_LITTER_WARNING_THRESHOLD = 23;
+    private static final int AGENT_DISGUST_WARNING_THRESHOLD = 22;
+    private static final int AGENT_VANDALISM_WARNING_THRESHOLD = 15;
+    private static final int AGENT_NOEXIT_WARNING_THRESHOLD = 8;
+    private static final int AGENT_LOST_WARNING_THRESHOLD = 8;
+
+    private static final int AGENT_MAX_HAPPINESS = 100;
+    private static final int AGENT_MIN_ENERGY = 10;
+    private static final int AGENT_MAX_ENERGY = 100;
+    private static final int AGENT_MAX_HUNGER = 100;
+    private static final int AGENT_MAX_THIRST = 100;
+    private static final int AGENT_MAX_TOILET = 100;
+    private static final int AGENT_MAX_NAUSEA = 100;
+    
+    
+    
+}
---
+++ src/main/java/Idlethemeparkworld/model/agent/Visitor.java
@@ -0,0 +1,5 @@
+package Idlethemeparkworld.model.agent;
+
+public class Visitor {
+    private int x,y;
+}
