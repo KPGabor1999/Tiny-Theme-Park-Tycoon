c375f0c91c829b14ef5f2475b756ba10428fc229
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-03-22 04:15:23 +0100

    Added support for loading the placeholder assets

--- src/main/java/Idlethemeparkworld/misc/Assets.java
+++ src/main/java/Idlethemeparkworld/misc/Assets.java
@@ -1,5 +1,10 @@
 package Idlethemeparkworld.misc;
 
+import java.awt.Image;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Assets {
     public static enum Texture {
         BURGERJOINT,
@@ -17,14 +22,29 @@ public class Assets {
         TRASHCAN
         ;
 
-        private String filename;        
+        private String filename;     
+        private Image asset;
 
         Texture(String path) {
             this.filename = path;
+            load();
         }
 
         Texture() {
             this.filename = name().replace("_", "").toLowerCase();
+            load();
         }
+        
+        private void load(){
+            try {
+                asset = ResourceLoader.loadImage(filename);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        private Image getAsset(){
+            return asset;
+        } 
     }
 }
