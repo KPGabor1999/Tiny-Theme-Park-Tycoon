e29dd582b7c6287f120fb397033725547178b19a
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-04-05 18:08:21 +0200

    shortened clean method

--- src/main/java/Idlethemeparkworld/model/agent/Janitor.java
+++ src/main/java/Idlethemeparkworld/model/agent/Janitor.java
@@ -2,24 +2,17 @@ package Idlethemeparkworld.model.agent;
 
 import Idlethemeparkworld.model.AgentManager;
 import Idlethemeparkworld.model.Park;
-import Idlethemeparkworld.model.Tile;
 import Idlethemeparkworld.model.Time;
 import Idlethemeparkworld.model.Updatable;
 import Idlethemeparkworld.model.agent.AgentInnerLogic.AgentActionType;
 import Idlethemeparkworld.model.agent.AgentInnerLogic.AgentState;
 import Idlethemeparkworld.model.buildable.Building;
-import Idlethemeparkworld.model.buildable.infrastucture.Entrance;
 import Idlethemeparkworld.model.buildable.infrastucture.Infrastructure;
-import Idlethemeparkworld.model.buildable.infrastucture.Pavement;
 import Idlethemeparkworld.model.buildable.infrastucture.Toilet;
 import Idlethemeparkworld.model.buildable.infrastucture.TrashCan;
 import java.awt.Color;
 import java.util.ArrayList;
 
-/**
- *
- * @author KrazyXL
- */
 public class Janitor extends Agent implements Updatable{
     private int salary;     //dollars per hour
     
@@ -49,46 +42,29 @@ public class Janitor extends Agent implements Updatable{
                 updateState();
             }
         }
-        
-        //Minden egész órakor levonjuk az órabérét. (AgentManager-ben)
     }
     
     private void updateState(){
         switch(state){
             case ENTERINGPARK:
                 setState(AgentState.IDLE);
-                currentAction = new AgentAction(AgentActionType.ENTERPARK, null);
-                //System.out.println("Beleptem a parkba.");
                 break;
             case IDLE:
                 setState(AgentState.WANDERING);
                 currentAction = new AgentAction(AgentActionType.WANDER, null);
-                //System.out.println("Setalni megyek.");
                 break;
-            //-------------------------------
             case WANDERING:
-                //Minden körben randomra rálép egy új mezõre és kitakarítja azt.
                 currentAction = new AgentAction(AgentActionType.WANDER, null);
-                //System.out.println("Továbbsetalok.");
-                break;
-            case QUEUING:
-                //System.err.println("Én nem kéne, hogy sorban álljak.");
-                //A mosdók takarításakor állhat sorban.
-                //De õ akár a végtelenségig is vár.
                 break;
             case CLEANING:
-                //Kitakarítja az adott mezõt, majd visszaáll Wandering-re.
                 currentAction = new AgentAction(AgentActionType.STAFFCLEAN, null);
-                //System.out.println("Ez a hely takaritast igenyel.");
                 break;
             case FLOATING:
-                //System.err.println("Én nem kéne, hogy lebegjek.");
                 if(statusTimer>Time.convMinuteToTick(5)){
                     remove();
                 }
                 break;
             default:
-                //System.err.println("Miért vagyok " + getState() + " állapotban?");
                 break;
         }
     }
@@ -96,9 +72,6 @@ public class Janitor extends Agent implements Updatable{
     private void performAction(long tickCount){
         if(currentAction != null){
             switch (currentAction.getAction()){
-                case ENTERPARK:
-                    enterCycle();
-                    break;
                 case WANDER:
                     //Átlép egy környezõ mezõre, ami nem fû vagy lockedTile.
                     //Frissítjük a currentBuilding-et.
@@ -115,19 +88,10 @@ public class Janitor extends Agent implements Updatable{
                     clean(currentBuilding);
                     setState(AgentState.WANDERING);
                     break;
-                case TOILET:
-                    //System.err.println("Nekem nem is kéne a vécén ülnöm.");
-                    //toiletCycle();
-                    break;
                 default:
-                    //System.err.println("Miért csinálom ezt: " + getCurrentAction() + "?");
                     break;
             }
         }
-    }
-    
-    private void enterCycle(){
-        Entrance entrance = (Entrance) currentBuilding;
     }
     
     private void moveToRandomNeighbourTile(){
@@ -139,20 +103,11 @@ public class Janitor extends Agent implements Updatable{
     }
     
     private void clean(Building currentBuilding){
-        if(currentBuilding instanceof Entrance){
-            ((Entrance)currentBuilding).setLittering(0);
-            System.out.println("Felszedtem a szemetet a bejáratnál.");
-        } else if(currentBuilding instanceof Pavement){
-            ((Pavement)currentBuilding).setLittering(0);
-            System.out.println("Felszedtem a szemetet egy járdáról.");
-        } else if(currentBuilding instanceof Toilet){
-            ((Toilet)currentBuilding).setLittering(0);
+        ((Infrastructure)currentBuilding).setLittering(0);
+        if(currentBuilding instanceof Toilet){
             ((Toilet)currentBuilding).setCleanliness(100);
-            System.out.println("Kitakarítottam egy mosdót.");
         } else if(currentBuilding instanceof TrashCan){
-            ((TrashCan)currentBuilding).setLittering(0);
             ((TrashCan)currentBuilding).setFilled(0);
-            System.out.println("Kiürítettem egy szemetest.");
         }
     }
     
