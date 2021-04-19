7837008cd8343bb230b72973466adb58ca54fa5c
PAL_GABOR <h6n8xs@inf.elte.hu>
2021-04-19 13:35:35 +0200

    A karbantartók unittesztjei is készen vannak.

--- src/test/java/MaintainerTest.java
+++ src/test/java/MaintainerTest.java
@@ -1,11 +1,17 @@
 import Idlethemeparkworld.model.BuildType;
 import Idlethemeparkworld.model.Park;
 import static Idlethemeparkworld.model.agent.AgentInnerLogic.AgentState.ENTERINGPARK;
+import static Idlethemeparkworld.model.agent.AgentInnerLogic.AgentState.FIXING;
 import static Idlethemeparkworld.model.agent.AgentInnerLogic.AgentState.IDLE;
 import static Idlethemeparkworld.model.agent.AgentInnerLogic.AgentState.WANDERING;
 import Idlethemeparkworld.model.agent.Maintainer;
 import Idlethemeparkworld.model.buildable.Building;
 import Idlethemeparkworld.model.buildable.attraction.Attraction;
+import Idlethemeparkworld.model.buildable.attraction.Carousel;
+import Idlethemeparkworld.model.buildable.attraction.FerrisWheel;
+import Idlethemeparkworld.model.buildable.attraction.HauntedMansion;
+import Idlethemeparkworld.model.buildable.attraction.RollerCoaster;
+import Idlethemeparkworld.model.buildable.attraction.SwingingShip;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,19 +25,15 @@ import static org.junit.Assert.*;
  */
 public class MaintainerTest {
     Park park;
-    Building attraction;
+    Building currentBuilding;
     Maintainer maintainer;
     
-    @BeforeClass
+    @Before
     public void initPark(){
-        //Inicializálj egy parkot, amiben csak egy Attrakció van.
+        //Inicializálj egy parkot.
         park = new Park(1,1,null);
-        park.build(BuildType.ROLLERCOASTER, 0, 0, true);
-        //Az attrakció legyen totálkáros legyen teleszemetelve.
-        attraction = park.getTile(0,0).getBuilding();
-        ((Attraction)attraction).setCondition(0);
         //Oda rakd le a karbantartót.
-        maintainer = new Maintainer(null, null, null);
+        maintainer = new Maintainer(null, park, null);
     }
     
     public void correctLifeCycle(){
@@ -41,10 +43,43 @@ public class MaintainerTest {
         assertEquals(IDLE, maintainer.getState());
         maintainer.update(0);
         assertEquals(WANDERING, maintainer.getState());
-        //Várd meg még kitakarítja a bejáratot, majd ellenõrizd, hogy tényleg tiszta lett-e.
+        //Megjavítja a körhintát?
+        park.build(BuildType.CAROUSEL, 0, 0, true);
+        currentBuilding = park.getTile(0,0).getBuilding();
+        ((Attraction)currentBuilding).setCondition(0);
         maintainer.update(0);
-        assertEquals(0, ((Attraction)attraction).getCondition());
-        //Ellenõrizd, hogy a takarító visszaállt-e WANDERING-be.
+        assertEquals(FIXING, maintainer.getState());    //Észreveszi, hogy az attrakció javításra szorul.
+        maintainer.update(0);                           //Megjavítja.
+        assertEquals(0, (int)((Carousel)currentBuilding).getCondition());
+        //Megjavítja az óriáskereket?
+        park.build(BuildType.FERRISWHEEL, 0, 0, true);
+        currentBuilding = park.getTile(0,0).getBuilding();
+        ((Attraction)currentBuilding).setCondition(0);
+        maintainer.update(0);
+        maintainer.update(0);
+        assertEquals(0, (int)((FerrisWheel)currentBuilding).getCondition());
+        //Megjavítja a rémségek házát?
+        park.build(BuildType.HAUNTEDMANSION, 0, 0, true);
+        currentBuilding = park.getTile(0,0).getBuilding();
+        ((Attraction)currentBuilding).setCondition(0);
+        maintainer.update(0);
+        maintainer.update(0);
+        assertEquals(0, (int)((HauntedMansion)currentBuilding).getCondition());
+        //Megjavítja a hullámvasutat?
+        park.build(BuildType.ROLLERCOASTER, 0, 0, true);
+        currentBuilding = park.getTile(0,0).getBuilding();
+        ((Attraction)currentBuilding).setCondition(0);
+        maintainer.update(0);
+        maintainer.update(0);
+        assertEquals(0, (int)((RollerCoaster)currentBuilding).getCondition());
+        //Megjavítja a hajóhintát?
+        park.build(BuildType.SWINGINGSHIP, 0, 0, true);
+        currentBuilding = park.getTile(0,0).getBuilding();
+        ((Attraction)currentBuilding).setCondition(0);
+        maintainer.update(0);
+        maintainer.update(0);
+        assertEquals(0, (int)((SwingingShip)currentBuilding).getCondition());
+        //Ellenõrizd, hogy a javítások után visszaáll-e WANDERING-be.
         assertEquals(WANDERING, maintainer.getState());
     }
     
