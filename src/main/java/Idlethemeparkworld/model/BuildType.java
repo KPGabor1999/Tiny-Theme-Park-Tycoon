ce3696f8eb13c9ee0b435b9623f1bc2398b02b08
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-04-21 04:45:41 +0200

    Added lobby locations

--- src/main/java/Idlethemeparkworld/model/BuildType.java
+++ src/main/java/Idlethemeparkworld/model/BuildType.java
@@ -15,37 +15,37 @@ import Idlethemeparkworld.model.buildable.infrastucture.LockedTile;
 import Idlethemeparkworld.model.buildable.infrastucture.Pavement;
 import Idlethemeparkworld.model.buildable.infrastucture.Toilet;
 import Idlethemeparkworld.model.buildable.infrastucture.TrashCan;
+import java.awt.Rectangle;
 
 public enum BuildType {
-    PAVEMENT(1, 1, "Pavement", "Use pavement tiles to connect buildings and help people get around.", 1000, Assets.Texture.PAVEMENT, 0, 0),
-    TRASHCAN(1, 1, "Trash can", "", 1000, Assets.Texture.TRASHCAN, 0, 0),
-    TOILET(1, 1, "Toilet", "", 5000, Assets.Texture.TOILET, 0, 0),
-    ICECREAMPARLOR(2, 1, "Ice Cream parlor", "", 10000, Assets.Texture.ICECREAMPARLOR, 25, 10),
-    HOTDOGSTAND(2, 1, "Hot Dog stand", "", 15000, Assets.Texture.HOTDOGSTAND, 50, 20),
-    BURGERJOINT(2, 1, "Burger joint", "", 25000, Assets.Texture.BURGERJOINT, 50, 20),
-    CAROUSEL(2, 2, "Carousel", "", 16000, Assets.Texture.CAROUSEL, 20, 20),
-    SWINGINGSHIP(3, 2, "Swinging ship", "", 32000, Assets.Texture.SWINGINGSHIP, 10, 20),
-    HAUNTEDMANSION(3, 2, "Haunted mansion", "", 32000, Assets.Texture.HAUNTEDMANSION, 80, 80),
-    FERRISWHEEL(3, 3, "Ferriswheel", "", 48000, Assets.Texture.FERRISWHEEL, 78, 78),
-    ROLLERCOASTER(3, 3, "Roller coaster", "", 60000, Assets.Texture.ROLLERCOASTER, 90, 90),
-    ENTRANCE(1, 1, "Entrance", "", 0, Assets.Texture.GATE, 0, 0),
-    LOCKEDTILE(1, 1, "Locked", "Unlock to use it as a building ground.", 5000, Assets.Texture.LOCKED, 0, 0);
+    PAVEMENT(1, 1, "Pavement", "Use pavement tiles to connect buildings and help people get around.", 1000, Assets.Texture.PAVEMENT, null),
+    TRASHCAN(1, 1, "Trash can", "", 1000, Assets.Texture.TRASHCAN, null),
+    TOILET(1, 1, "Toilet", "", 5000, Assets.Texture.TOILET, new Rectangle(0,40,64,24)),
+    ICECREAMPARLOR(2, 1, "Ice Cream parlor", "", 10000, Assets.Texture.ICECREAMPARLOR, new Rectangle(0,0,75,64)),
+    HOTDOGSTAND(2, 1, "Hot Dog stand", "", 15000, Assets.Texture.HOTDOGSTAND, new Rectangle(54,5,74,59)),
+    BURGERJOINT(2, 1, "Burger joint", "", 25000, Assets.Texture.BURGERJOINT, new Rectangle(34,0,94,64)),
+    CAROUSEL(2, 2, "Carousel", "", 16000, Assets.Texture.CAROUSEL, new Rectangle(0,84,128,44)),
+    SWINGINGSHIP(3, 2, "Swinging ship", "", 32000, Assets.Texture.SWINGINGSHIP, new Rectangle(0,96,128,32)),
+    HAUNTEDMANSION(3, 2, "Haunted mansion", "", 32000, Assets.Texture.HAUNTEDMANSION, new Rectangle(0,86,128,42)),
+    FERRISWHEEL(3, 3, "Ferriswheel", "", 48000, Assets.Texture.FERRISWHEEL, new Rectangle(0,138,192,54)),
+    ROLLERCOASTER(3, 3, "Roller coaster", "", 60000, Assets.Texture.ROLLERCOASTER, new Rectangle(0,0,64,128)),
+    ENTRANCE(1, 1, "Entrance", "", 0, Assets.Texture.GATE, null),
+    LOCKEDTILE(1, 1, "Locked", "Unlock to use it as a building ground.", 5000, Assets.Texture.LOCKED, null);
 
     private final int width, length;
     private final String name, description;
     private final int buildCost;
     private final Texture tex;
-    private final int queueX, queueY;
+    private final Rectangle lobbyArea;
 
-    BuildType(int w, int l, String name, String description, int bc, Texture tex, int qX, int qY) {
+    BuildType(int w, int l, String name, String description, int bc, Texture tex, Rectangle lobbyArea) {
         this.width = w;
         this.length = l;
         this.name = name;
         this.description = description;
         this.buildCost = bc;
         this.tex = tex;
-        this.queueX = qX;
-        this.queueY = qY;
+        this.lobbyArea = lobbyArea;
     }
 
     public static Class GetClass(BuildType type) {
@@ -105,11 +105,7 @@ public enum BuildType {
         return tex;
     }
     
-    public int getQueueX(){
-        return queueX;
-    }
-    
-    public int getQueueY(){
-        return queueY;
+    public Rectangle getLobbyArea() {
+        return lobbyArea;
     }
 }
