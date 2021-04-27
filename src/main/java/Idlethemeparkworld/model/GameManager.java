c2209024159f53b444fc4e4d470f9d7c150267bf
PAL_GABOR <h6n8xs@inf.elte.hu>
2021-04-27 13:20:46 +0200

    A játékos kezdjen 50000 dollárral és az órabér óránként vonódik le

--- src/main/java/Idlethemeparkworld/model/GameManager.java
+++ src/main/java/Idlethemeparkworld/model/GameManager.java
@@ -47,7 +47,7 @@ public class GameManager {
         this.gameFroze = false;
         this.park = new Park(10, 10, this);
         this.time = new Time();
-        this.finance = new Finance(1000000);       //Normálisan 50K
+        this.finance = new Finance(50000);       //Normálisan 50K
         this.am = new AgentManager(park, this);
         this.stats = new Statistics(this);
         this.board = null;
