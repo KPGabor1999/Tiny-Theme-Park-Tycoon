245801770ee2a972c2a5eb68fd87be56e3ecf5fc
Tran Bach Khoa <saof2t@inf.elte.hu>
2021-05-08 00:28:54 +0200

    Added loan system

--- src/main/java/Idlethemeparkworld/model/administration/Finance.java
+++ src/main/java/Idlethemeparkworld/model/administration/Finance.java
@@ -1,9 +1,12 @@
 package Idlethemeparkworld.model.administration;
 
+import Idlethemeparkworld.misc.utils.Range;
+import Idlethemeparkworld.model.Time;
+import Idlethemeparkworld.model.Updatable;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
-public class Finance {
+public class Finance implements Updatable {
     
     public enum FinanceType {
         RIDE_SELL("Attraction sales"),
@@ -14,7 +17,9 @@ public class Finance {
         SALARY("Salaries"),
         UPKEEP("Upkeep costs"),
         BUILDING("Building costs"),
-        UPGRADE("Upgrade costs");
+        UPGRADE("Upgrade costs"),
+        LOANED("Loaned bonus"),
+        LOAN_PAYBACK("Loan payback costs");
         
         String name;
         
@@ -29,6 +34,14 @@ public class Finance {
 
     private int initialFunds;
     private int funds;
+    
+    private static final Range dayDurationRange = new Range(5,20);
+    private int principal;
+    private int calculatedPay;
+    private int dailyFee;
+    private double interestRate;
+    private int paybackDuration;
+    private int dayProgress;
 
     LinkedHashMap<FinanceType, Integer> financialData;
     
@@ -55,6 +68,8 @@ public class Finance {
         for (FinanceType type : FinanceType.values()) {
             financialData.put(type, 0);
         }
+        
+        principal = -1;
     }
     
     /**
@@ -113,6 +128,84 @@ public class Finance {
         sb.append("--------").append("\n");
         financialData.forEach((k, v) -> sb.append(k.getName()).append(": ").append(v).append("\n"));
         return sb.toString();
+    }
+
+    public static Range getDayDurationRange() {
+        return dayDurationRange;
+    }
+
+    public int getPrincipal() {
+        return principal;
+    }
+
+    public int getCalculatedPay() {
+        return calculatedPay;
+    }
+
+    public int getDailyFee() {
+        return dailyFee;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public int getPaybackDuration() {
+        return paybackDuration;
+    }
+
+    public int getDayProgress() {
+        return dayProgress;
+    }
+    
+    private Range getLoanRange(){
+        Range range = new Range(0,0);
+        range.setRange(Math.max((int)Math.floor(funds*0.1), 10000),
+                        Math.min((int)Math.floor(funds), 250000));
+        return range;
+    }
+    
+    public double getInterestRate(int loanAmount, int dayDuration){
+        Range range = getLoanRange();
+        int normalizedRange = range.getHigh()-range.getLow();
+        int normalizedLoan = loanAmount-range.getLow();
+        double rate = 1.05;
+        rate += 0.25*normalizedLoan/normalizedRange;
+        rate += 0.15*(dayDuration-dayDurationRange.getLow())/(dayDurationRange.getHigh()-dayDurationRange.getLow());
+        return rate;
+    }
+    
+    public boolean getLoan(int amount, int dayDuration ){
+        Range range = getLoanRange();
+        if(range.inRange(amount)){
+            interestRate = getInterestRate(amount, dayDuration);
+            principal = amount;
+            calculatedPay = (int)Math.floor(interestRate*5*principal);
+            dailyFee = Math.floorDiv(calculatedPay, dayDuration);
+            paybackDuration = dayDuration;
+            earn(amount, FinanceType.LOANED);
+            dayProgress = 0;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void paybackLoan(){
+        pay(dailyFee, FinanceType.LOAN_PAYBACK);
+        dayProgress++;
+        if(paybackDuration <= dayProgress){
+            principal = -1;
+        }
+    }
+    
+    @Override
+    public void update(long tickCount) {
+        if (tickCount % Time.convMinuteToTick(10) == 0) {
+            if(principal != -1){
+                paybackLoan();
+            }
+        }
     }
     
     @Override
